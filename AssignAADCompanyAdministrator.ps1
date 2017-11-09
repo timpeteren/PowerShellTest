b2b548f34045de1dd4afc8e49f472fed159dfb24
Tim Peter Edstrøm <22084975+timpeteren@users.noreply.github.com>
2017-11-09 15:25:06 +0100

    AssignAADCompanyAdmin + ClientCredentials

---
+++ AssignAADCompanyAdministrator.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzureAD
+#whichever is easier, appName or appOID
+#$appOID = "37e91fb7-2bf2-4087-8c5c-02b23f670d1a"
+$app = Get-AzureADServicePrincipal -SearchString "GraphAPI-B2C-CRU-App"
+$role = Get-AzureADDirectoryRole | Where-Object { $_.DisplayName -eq "Company Administrator" }
+Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $app.ObjectId
--- ClientCredentialAndPasswordGrant.ps1
+++ ClientCredentialAndPasswordGrant.ps1
@@ -3,22 +3,22 @@
 #####################################################
 
 # Your Client ID and Client Secret obainted when registering your WebApp
-$clientid = ''
+$clientId = ''
 $clientSecret = ''
-$TenantName = '.onmicrosoft.com'
+$tenantName = '.onmicrosoft.com'
 $resource = 'https://graph.windows.net' 
-$GrantType = "client_credentials"
-$Uri = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token" -f $TenantName
+$grantType = "client_credentials"
+$uri = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token" -f $tenantName
 $scopeFormatted = "{0}" -f $resource
 
-$Body = @{
-    "grant_type" = $GrantType
-    "client_id" = $clientid
+$body = @{
+    "grant_type" = $grantType
+    "client_id" = $clientId
     "scope" = $scopeFormatted
     "client_secret" = $clientSecret
 }
 
-$token = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body -ContentType "application/x-www-form-urlencoded"
+$token = Invoke-RestMethod -Uri $uri -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
 Write-Output $token
 
 #####################################################
@@ -26,17 +26,17 @@ Write-Output $token
 #####################################################
 
 # Your Client ID and Client Secret obainted when registering your WebApp
-$clientid = '' # 37e91fb7-2bf2-4087-8c5c-02b23f670d1a # GraphAPI-CRU-App
+$clientId = '' # 37e91fb7-2bf2-4087-8c5c-02b23f670d1a # GraphAPI-CRU-App
 $clientSecret = '' 
-$TenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
+$tenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
 $resource = 'https://graph.windows.net/'
-$GrantType = "password"
-$username = "admin@{0}" -f $TenantName
+$grantType = "password"
+$username = "admin@{0}" -f $tenantName
 $password = "xxxxxxxxx"
-$Uri = "https://login.microsoftonline.com/{0}/oauth2/token" -f $TenantName
+$uri = "https://login.microsoftonline.com/{0}/oauth2/token" -f $tenantName
 
-$Body = @{
-    "grant_type" = $GrantType
+$body = @{
+    "grant_type" = $grantType
     "client_id" = $clientid
     "resource" = $resource
     "client_secret" = $clientSecret
@@ -44,7 +44,7 @@ $Body = @{
     "password" = $password
 }
 
-$token = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body -ContentType "application/x-www-form-urlencoded"
+$token = Invoke-RestMethod -Uri $uri -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
 Write-Output $token
 
 #####################################################
@@ -52,55 +52,55 @@ Write-Output $token
 #####################################################
 
 # Your Client ID and Client Secret obainted when registering your WebApp
-$clientid = '' # 37e91fb7-2bf2-4087-8c5c-02b23f670d1a # GraphAPI-CRU-App
+$clientId = '' # 37e91fb7-2bf2-4087-8c5c-02b23f670d1a # GraphAPI-CRU-App
 $clientSecret = ''
-$TenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
+$tenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
 $resource = 'https://graph.windows.net/'
-$GrantType = "client_credentials"
-$Uri = "https://login.microsoftonline.com/{0}/oauth2/token" -f $TenantName
+$grantType = "client_credentials"
+$uri = "https://login.microsoftonline.com/{0}/oauth2/token" -f $tenantName
 
 $Body = @{
-    "grant_type" = $GrantType
-    "client_id" = $clientid
+    "grant_type" = $grantType
+    "client_id" = $clientId
     "resource" = $resource
     "client_secret" = $clientSecret
 }
 
-$token = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body -ContentType "application/x-www-form-urlencoded"
+$token = Invoke-RestMethod -Uri $uri -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
 Write-Output $token
 
 
 # ------------------NEXT STEP, two examples, GET, POST and PATCH------------------ #
 
 # List users in tenant example using -Method GET
-$AuthHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
-$TenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
-$GraphUri = "https://graph.windows.net/{0}/users/?api-version=1.6" -f $TenantName
+$authHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
+$tenantName = '.onmicrosoft.com' # laerdalmedicalb2ctest
+$graphUri = "https://graph.windows.net/{0}/users/?api-version=1.6" -f $tenantName
 
-Invoke-RestMethod -Method Get -Uri $GraphUri -Headers $AuthHeader -ContentType "application/json"
+Invoke-RestMethod -Method Get -Uri $graphUri -Headers $authHeader -ContentType "application/json"
 
 
 # Application schema extension example using -Method POST
-$AuthHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
-$TenantName = '.onmicrosoft.com'
-$ApplicationObjectId = "" # b5013aaf-16bf-4ab2-972b-e01be641dfc0 # GraphAPI-Extensions-App
-$GraphUri = "https://graph.windows.net/{0}/applications/{1}/extensionProperties?api-version=1.6" -f $TenantName, $ApplicationObjectId
-$Body = @{
+$authHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
+$tenantName = '.onmicrosoft.com'
+$applicationObjectId = "" # b5013aaf-16bf-4ab2-972b-e01be641dfc0 # GraphAPI-Extensions-App
+$graphUri = "https://graph.windows.net/{0}/applications/{1}/extensionProperties?api-version=1.6" -f $tenantName, $applicationObjectId
+$body = @{
     name = 'newCustomAttribute'
     dataType = 'String'
     targetObjects = @("User")
 }
 
-Invoke-RestMethod -Method Post -Uri $GraphUri -Body ($Body | ConvertTo-Json) -Headers $AuthHeader -ContentType "application/json"
+Invoke-RestMethod -Method Post -Uri $graphUri -Body ($body | ConvertTo-Json) -Headers $authHeader -ContentType "application/json"
 
 
 # Modify user attribute example using -Method PATCH
-$AuthHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
-$TenantName = '.onmicrosoft.com'
-$UserObjectId = "" # "f65af661-3c33-474f-a3df-eddeafeb7a3e" # tpe@gmail.com
-$GraphUri = "https://graph.windows.net/{0}/users/{1}/?api-version=1.6" -f $TenantName, $UserObjectId
+$authHeader = @{Authorization = "Bearer {0}" -f $token.access_token}
+$tenantName = '.onmicrosoft.com'
+$userObjectId = "" # "f65af661-3c33-474f-a3df-eddeafeb7a3e" # tpe@gmail.com
+$graphUri = "https://graph.windows.net/{0}/users/{1}/?api-version=1.6" -f $tenantName, $userObjectId
 
-$Body = @{
+$body = @{
     signInNames = @(
         @{
             type = "emailAddress"
@@ -113,4 +113,4 @@ $Body = @{
     )    
 }
 
-Invoke-RestMethod -Method Patch -Uri $GraphUri -Body ($body | ConvertTo-Json) -Headers $AuthHeader -ContentType "application/json"
+Invoke-RestMethod -Method Patch -Uri $graphUri -Body ($body | ConvertTo-Json) -Headers $authHeader -ContentType "application/json"
