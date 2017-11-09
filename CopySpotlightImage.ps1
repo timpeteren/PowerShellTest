90f6f62d496ee42afe39d4636ac5d430250e9c78
Tim Peter Edstrøm <22084975+timpeteren@users.noreply.github.com>
2017-11-09 23:10:29 +0100

    CopySpotlightImage.ps1

---
+++ CopySpotlightImage.ps1
@@ -0,0 +1,49 @@
+﻿Function Get-FileMetaData 
+{ 
+Param([string[]]$folder) 
+foreach($sFolder in $folder) 
+    { 
+    $a = 0 
+    $objShell = New-Object -ComObject Shell.Application 
+    $objFolder = $objShell.namespace($sFolder) 
+
+    foreach ($File in $objFolder.items()) 
+    {  
+    $FileMetaData = New-Object PSOBJECT 
+        for ($a ; $a  -le 266; $a++) 
+        {  
+        if($objFolder.getDetailsOf($File, $a)) 
+            { 
+            $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))  = 
+                    $($objFolder.getDetailsOf($File, $a)) } 
+            $FileMetaData | Add-Member $hash 
+            $hash.clear()  
+            } #end if 
+        } #end for  
+    $a=0 
+    $FileMetaData 
+    } #end foreach $file 
+    } #end foreach $sfolder 
+} #end Get-FileMetaData
+
+
+### kopierer filer fra $assetPath til $tempPath og omdøper dem ved å legge til .jpg endelse
+## COPY .JPG from FOLDER A TO FOLDER B! ###
+## Errorcheck -> user locale (!) ###
+
+$assetPath = $env:USERPROFILE + "\" + 'AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\'
+$tempPath  = $env:USERPROFILE + "\" + 'Desktop\Temp\'
+$assetImgs = @(Get-ChildItem -Path $assetPath)
+
+for ($i = 0; $i -lt $assetImgs.Count; $i++) 
+    {
+    if ($assetImgs[$i].CreationTime -gt (Get-Date).AddDays(-1))
+        {
+        Copy-item $assetImgs[$i].FullName -Destination ($tempPath +  $assetImgs[$i] + ".jpg")
+        }
+    }
+
+$landPictures = Get-FileMetaData -folder $tempPath | Where-Object {$_.Bredde -like "*1920*"}
+$portPictures = Get-FileMetaData -folder $tempPath | Where-Object {$_.Bredde -like "*1080*"}
+foreach ($pic in $landPictures) { Copy-Item -Path ($pic.Filnavn + ".jpg") -Destination ($tempPath + "..\Spotlight") -Force }
+foreach ($pic in $portPictures) { Copy-Item -Path ($pic.Filnavn + ".jpg") -Destination ($tempPath + "..\Spotlight vertical") -Force }
